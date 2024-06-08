da7597a42f850d4726bbdce39e500d6e9e8c8a3c
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-06-08 14:38:24 +0530

    feat : configure font themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'neue-haas': ['Neue Haas Grotesk Display Pro', 'sans'],
+      },
+      fontSize: {
+          "section-title":[
+            '56px',
+            {
+              lineHeight: '60px',
+              letterSpacing :'-1px'
+    
+            }
+          ],
+        "section-subtitle":[
+          '19px',{
+              lineHeight: '28px',
+              letterSpacing :'0.5px'
+          }
+        ],
+        "bento-title" : '24px',
+        'bento-content' : '14px',
+        'bento-logo' : '60px'
+      },
+      backgroundColor:{
+        "primary" : "#010314"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        "section-subtitle-clr":'#DFE1F4'
+      }
     },
   },
   plugins: [],
